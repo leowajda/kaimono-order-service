14f3286ef96a3cbd9b08d1d8c95a66cc9816397c
Leonardo Wajda <dev.leowajda@tuta.io>
2023-03-01 20:32:33 +0100

    :tada: Start kaimono-order-service

---
+++ src/main/java/com/kaimono/order/service/KaimonoOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kaimono.order.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaimonoOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaimonoOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaimono/order/service/KaimonoOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaimono.order.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaimonoOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
