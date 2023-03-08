f05ef622570dd06528f0e4ed88fe6620e577ff36
Leonardo Wajda <dev.leowajda@tuta.io>
2023-03-08 17:10:03 +0100

    :sparkles: Add support for OpenID authentication and OAuth2.0 authorization

---
+++ src/main/java/com/kaimono/order/service/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.kaimono.order.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
+                .requestCache(ServerHttpSecurity.RequestCacheSpec::disable)
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
+    }
+
+}
